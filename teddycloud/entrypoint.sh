ad5cf797a5a805c11ec9cfb2060c9ce5bb3306ad
Sebastian Rutofski <kontakt@sebastian-rutofski.de>
2024-01-07 20:32:32 +0100

    add first teddycloud-ha version

---
+++ teddycloud/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# config
+SHARE_DIR=/addon_config
+
+# ha config dirs
+echo "creating config dirs"
+mkdir -p "$SHARE_DIR/certs"
+mkdir -p "$SHARE_DIR/config"
+
+# ha config symlinks
+echo "creating /teddycloud symlinks"
+
+#ln -sF /teddycloud/config /config/config
+rm -rf /teddycloud/certs/
+ln -sF "$SHARE_DIR/certs" /teddycloud/certs
+
+rm -rf /teddycloud/config/
+ln -sF "$SHARE_DIR/config" /teddycloud/config
+
+# run default entrypoint
+/docker-entrypoint.sh
